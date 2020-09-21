ebd1c48b4ce44b1021bd853bbd440177dd210808
Leonardo J. Ramirez <lramirez.army@gmail.com>
2020-09-21 17:47:44 -0400

    removed vuex customProducts

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import products from './modules/products';
-import customProducts from './modules/customProducts';
 import usersSnipcart from './modules/users.snipcart';
 import usersFirebase from './modules/users.firebase';
 
@@ -10,7 +9,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     products,
-    customProducts,
     usersSnipcart,
     usersFirebase
   }
